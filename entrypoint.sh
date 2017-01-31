419e0eb6e5c31e68aa495fac5551ed3ebfa7540a
Jon Schulberger <jschoulzy@gmail.com>
2017-01-31 12:30:05 -0500

    Update and rename startscript.sh to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
-
-
 #!/bin/bash
 
 set -e
-
-# Make sure the PID file does not exist
-rm -rf /mnt/deluge/deluged/pid
 
 # Start the daemon
 deluged -c /mnt/deluge -L info -l /mnt/deluge/deluged.log
